90695798ffdb558d77bfb848c8329d879e9ecbbd
Ryan Dang <dangryan@ca.ibm.com>
2018-02-25 18:53:51 -0500

    Update some stuffs

--- src/app/about-me/about-me.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-  selector: 'app-about-me',
-  templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss'],
-  encapsulation: ViewEncapsulation.None
-})
-export class AboutMeComponent {}
--- src/app/top-nav/top-nav.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-  selector: 'top-nav',
-  templateUrl: './top-nav.component.html',
-  styleUrls: ['./top-nav.component.scss'],
-  encapsulation: ViewEncapsulation.None
-})
-export class TopNavComponent {}
